f15e96c68acb7ee4be6f16d1049e5ece664f2537
imannudiniman1-art <imannudiniman1@gmail.com>
2026-08-10 18:41:50 +0700

    Update knowledge.py

--- src/knowledge.py
+++ src/knowledge.py
@@ -1,1 +1,21 @@
-knowledge.py
+"""
+ScholarMind Knowledge Module
+Initial representation of research knowledge.
+"""
+
+class KnowledgeItem:
+    def __init__(self, title, knowledge_type, description=""):
+        self.title = title
+        self.knowledge_type = knowledge_type
+        self.description = description
+
+    def summary(self):
+        return {
+            "title": self.title,
+            "type": self.knowledge_type,
+            "description": self.description
+        }
+
+
+def create_knowledge_item(title, knowledge_type, description=""):
+    return KnowledgeItem(title, knowledge_type, description)
