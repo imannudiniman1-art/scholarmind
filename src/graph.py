4ab54b02baaf97c2806bb031a477a146a62b4248
imannudiniman1-art <imannudiniman1@gmail.com>
2026-08-10 19:02:04 +0700

    Create graph.py

---
+++ src/graph.py
@@ -0,0 +1,35 @@
+"""
+ScholarMind Knowledge Graph Module
+Initial implementation of knowledge relationships.
+"""
+
+
+class KnowledgeGraph:
+    def __init__(self):
+        self.nodes = {}
+        self.relationships = []
+
+    def add_node(self, node_id, node_type, label):
+        self.nodes[node_id] = {
+            "type": node_type,
+            "label": label
+        }
+
+    def add_relationship(self, source, relation, target):
+        self.relationships.append({
+            "source": source,
+            "relation": relation,
+            "target": target
+        })
+
+    def get_nodes(self):
+        return self.nodes
+
+    def get_relationships(self):
+        return self.relationships
+
+    def count_nodes(self):
+        return len(self.nodes)
+
+    def count_relationships(self):
+        return len(self.relationships)
