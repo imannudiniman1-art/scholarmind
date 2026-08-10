12c69016da9c851e5f77dd73c34e742c75e875b9
imannudiniman1-art <imannudiniman1@gmail.com>
2026-08-10 18:43:37 +0700

    Update memory.py

--- src/memory.py
+++ src/memory.py
@@ -1,1 +1,21 @@
-memory.py
+"""
+ScholarMind Research Memory Module
+Initial implementation of research memory.
+"""
+
+class ResearchMemory:
+    def __init__(self):
+        self.items = []
+
+    def add(self, knowledge_item):
+        self.items.append(knowledge_item)
+
+    def get_all(self):
+        return self.items
+
+    def count(self):
+        return len(self.items)
+
+
+def create_memory():
+    return ResearchMemory()
